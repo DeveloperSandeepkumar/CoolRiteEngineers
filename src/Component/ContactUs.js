64aa13de89a12c6ca4ff9b646db318d555b013be
Sandeep Kumar <98761409+DeveloperSandeepkumar@users.noreply.github.com>
2026-04-04 11:02:19 +0530

    Update ContactUs.js

--- src/Component/ContactUs.js
+++ src/Component/ContactUs.js
@@ -209,21 +209,40 @@ const ContactUs = () => {
         </div>
       </div>
 
-      {/* Google Map */}
-   <h4 className="text-center mb-3">Our Location</h4>
-      <div className="container" style={styles.mapContainer}>
-     
-        <div className="ratio ratio-16x9">
-          <iframe
-            title="map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3421.813817872341!2d76.7758831!3d30.9455745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ff57de71ccde7%3A0xb513973c84f8c7e0!2sGoogle+Maps+Location!5e0!3m2!1sen!2sin!4v1700000000000"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
-      </div>
+{/* Google Map Section */}
+<div className="container my-5">
+  {/* Section Header */}
+  <div className="text-center mb-4">
+    <h2 style={{
+      fontWeight: '700',
+      fontSize: '2rem',
+      color: '#222',
+      marginBottom: '10px',
+      letterSpacing: '1px',
+    }}>
+      Our Location
+    </h2>
+    <p style={{
+      color: '#555',
+      fontSize: '1rem',
+      marginBottom: '30px',
+    }}>
+      Find our head office and branch office locations on the map below.
+    </p>
+  </div>
+
+  {/* Map */}
+  <div className="ratio ratio-16x9 shadow rounded">
+    <iframe
+      title="map"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3421.813817872341!2d76.7758831!3d30.9455745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ff57de71ccde7%3A0xb513973c84f8c7e0!2sGoogle+Maps+Location!5e0!3m2!1sen!2sin!4v1700000000000"
+      style={{ border: 0 }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+  </div>
+</div>
     </div>
   );
 };
