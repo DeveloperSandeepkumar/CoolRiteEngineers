7af2a1b6a9b422329d458404318d9b6757a64cd8
Sandeep Kumar <98761409+DeveloperSandeepkumar@users.noreply.github.com>
2026-04-23 11:22:50 +0530

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -3,7 +3,8 @@ import { NavLink } from 'react-router-dom'
 import './Footer.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { FaPhoneAlt, FaLongArrowAltRight } from "react-icons/fa";
+import { FaPhoneAlt, FaLongArrowAltRight, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { services } from "../Component/servicesData";
 
 const Footer = () => {
     return (
@@ -18,33 +19,29 @@ const Footer = () => {
                         <p>MEP consultancy with no delays & errors</p>
                         <hr />
 
-                        <div className="col-md-6 col-lg-6 col-xl-6 mx-auto mb-6">
+                        {/* Contact */}
+                        <div className="col-md-4 col-lg-4 col-xl-4 mx-auto mb-3">
                             <h6 className="text-uppercase fw-bold mb-4 textHeading">
                                 Quick Contact
                             </h6>
-
-                            <p className="textclass">
-                                If you have any questions, feel free to contact our team.
-                            </p>
 
                             <p className="textStyle">
                                 <FaPhoneAlt /> +91-7009167480 / +91-7973418255
                             </p>
 
                             <p className="textclass">
-                                <b>Email:</b>{" "}
-                                <span className="textStyle">
-                                   Coolriteengineers@gmail.com
-                                </span>
+                                <FaEnvelope /> Coolriteengineers@gmail.com
                             </p>
 
-                            <h4 className="textHeading">Head Office</h4>
                             <p className="textclass">
-                               Address:- SCO NO - 03 Village Malpur Upperla near Cipla Ltd. P.O Bhud , Teh Baddi Distt Solan (Himachal Pradesh -173205)
+                                <FaMapMarkerAlt /> SCO NO - 03 Village Malpur Upperla <br />
+                                Near Cipla Ltd. P.O Bhud, Baddi <br />
+                                Distt Solan (HP -173205)
                             </p>         
                         </div>
 
-                        <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mb-3">
+                        {/* Company */}
+                        <div className="col-md-4 col-lg-4 col-xl-4 mx-auto mb-3">
                             <h6 className="text-uppercase fw-bold mb-4 textHeading">
                                 Company
                             </h6>
@@ -58,18 +55,40 @@ const Footer = () => {
                             </p>
 
                             <p className="textStyle">
-                                <FaLongArrowAltRight /> <NavLink to="/Contact" className="text_Style">Contact Us</NavLink>
+                                <FaLongArrowAltRight /> <NavLink to="/Project" className="text_Style">Projects</NavLink>
+                            </p>
+
+                            <p className="textStyle">
+                                <FaLongArrowAltRight /> <NavLink to="/Career" className="text_Style">Career</NavLink>
+                            </p>
+
+                            <p className="textStyle">
+                                <FaLongArrowAltRight /> <NavLink to="/ContactUs" className="text_Style">Contact Us</NavLink>
                             </p>
                         </div>
 
-               
+                        {/* Services (NEW ADDED) */}
+                        <div className="col-md-4 col-lg-4 col-xl-4 mx-auto mb-3">
+                            <h6 className="text-uppercase fw-bold mb-4 textHeading">
+                                MEP Services
+                            </h6>
+
+                            {services.slice(0, 6).map((service, index) => (
+                                <p key={index} className="textStyle">
+                                    <FaLongArrowAltRight />{" "}
+                                    <NavLink to={service.path} className="text_Style">
+                                        {service.title}
+                                    </NavLink>
+                                </p>
+                            ))}
+                        </div>
 
                     </div>
                 </div>
             </section>
 
-            <div className="text-center p-4">
-                © 2025 Copyright:
+            <div className="text-center p-3">
+                © {new Date().getFullYear()} Copyright:
                 <a className="text-reset fw-bold" href="https://CoolRiteEngineers.com/">
                     CoolRiteEngineers.com
                 </a>
