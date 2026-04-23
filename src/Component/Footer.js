c6d401356a2553cf15a5c65d92690e3e1663c3f0
Sandeep Kumar <98761409+DeveloperSandeepkumar@users.noreply.github.com>
2026-04-23 11:20:50 +0530

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -3,87 +3,74 @@ import { NavLink } from 'react-router-dom'
 import './Footer.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { FaPhoneAlt, FaLongArrowAltRight, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
-import { services } from "../Component/servicesData";
+import { FaPhoneAlt, FaLongArrowAltRight } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <footer className="text-center text-lg-start bg-body-tertiary text-muted footer">
             <section className="bg-dark text-white">
-                <div className="container text-center text-md-start mt-4 textdiv">
+                <div className="container text-center text-md-start mt-5 textdiv">
                     <div className="row mt-3">
 
-                        {/* Top Heading */}
-                        <div className="col-12 mb-3">
-                            <p style={{ fontSize: '20px', fontWeight: 'bold' }}>
-                                Most Innovative MEP Consultants
-                            </p>
-                            <p style={{ fontSize: '13px' }}>
-                                MEP consultancy with no delays & errors
-                            </p>
-                            <hr />
-                        </div>
+                        <p style={{ fontSize: '24px', fontWeight: 'bold' }}>
+                            Most Innovative MEP Consultants.
+                        </p>
+                        <p>MEP consultancy with no delays & errors</p>
+                        <hr />
 
-                        {/* Contact */}
-                        <div className="col-md-4 col-lg-4 mb-3">
-                            <h6 className="text-uppercase fw-bold mb-3 textHeading">
+                        <div className="col-md-6 col-lg-6 col-xl-6 mx-auto mb-6">
+                            <h6 className="text-uppercase fw-bold mb-4 textHeading">
                                 Quick Contact
                             </h6>
 
                             <p className="textclass">
-                                <FaPhoneAlt /> +91-7009167480 <br />
-                                <FaPhoneAlt /> +91-7973418255
+                                If you have any questions, feel free to contact our team.
+                            </p>
+
+                            <p className="textStyle">
+                                <FaPhoneAlt /> +91-7009167480 / +91-7973418255
                             </p>
 
                             <p className="textclass">
-                                <FaEnvelope /> Coolriteengineers@gmail.com
+                                <b>Email:</b>{" "}
+                                <span className="textStyle">
+                                   Coolriteengineers@gmail.com
+                                </span>
                             </p>
 
-                            <p className="textclass" style={{ fontSize: '13px' }}>
-                                <FaMapMarkerAlt /> SCO NO - 03 Village Malpur Upperla <br />
-                                Near Cipla Ltd., P.O Bhud, Baddi <br />
-                                Distt Solan (HP - 173205)
-                            </p>
+                            <h4 className="textHeading">Head Office</h4>
+                            <p className="textclass">
+                               Address:- SCO NO - 03 Village Malpur Upperla near Cipla Ltd. P.O Bhud , Teh Baddi Distt Solan (Himachal Pradesh -173205)
+                            </p>         
                         </div>
 
-                        {/* Company Links */}
-                        <div className="col-md-4 col-lg-4 mb-3">
-                            <h6 className="text-uppercase fw-bold mb-3 textHeading">
+                        <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mb-3">
+                            <h6 className="text-uppercase fw-bold mb-4 textHeading">
                                 Company
                             </h6>
 
-                            <p><FaLongArrowAltRight /> <NavLink to="/" className="text_Style">Home</NavLink></p>
-                            <p><FaLongArrowAltRight /> <NavLink to="/About" className="text_Style">About</NavLink></p>
-                            <p><FaLongArrowAltRight /> <NavLink to="/Project" className="text_Style">Projects</NavLink></p>
-                            <p><FaLongArrowAltRight /> <NavLink to="/Career" className="text_Style">Career</NavLink></p>
-                            <p><FaLongArrowAltRight /> <NavLink to="/ContactUs" className="text_Style">Contact Us</NavLink></p>
+                            <p className="textStyle">
+                                <FaLongArrowAltRight /> <NavLink to="/" className="text_Style">Home</NavLink>
+                            </p>
+
+                            <p className="textStyle">
+                                <FaLongArrowAltRight /> <NavLink to="/About" className="text_Style">About</NavLink>
+                            </p>
+
+                            <p className="textStyle">
+                                <FaLongArrowAltRight /> <NavLink to="/Contact" className="text_Style">Contact Us</NavLink>
+                            </p>
                         </div>
 
-                        {/* Services (NEW) */}
-                        <div className="col-md-4 col-lg-4 mb-3">
-                            <h6 className="text-uppercase fw-bold mb-3 textHeading">
-                                MEP Services
-                            </h6>
-
-                            {services.slice(0, 6).map((service, index) => (
-                                <p key={index}>
-                                    <FaLongArrowAltRight />{" "}
-                                    <NavLink to={service.path} className="text_Style">
-                                        {service.title}
-                                    </NavLink>
-                                </p>
-                            ))}
-
-                        </div>
+               
 
                     </div>
                 </div>
             </section>
 
-            {/* Bottom */}
-            <div className="text-center p-3" style={{ fontSize: '13px' }}>
-                © {new Date().getFullYear()} Copyright:
-                <a className="text-reset fw-bold ms-1" href="https://CoolRiteEngineers.com/">
+            <div className="text-center p-4">
+                © 2025 Copyright:
+                <a className="text-reset fw-bold" href="https://CoolRiteEngineers.com/">
                     CoolRiteEngineers.com
                 </a>
             </div>
